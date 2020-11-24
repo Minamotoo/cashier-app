0ed61c7cf0af81354a62b65e554ce0112a0b01bd
PauL <cee.0011@gmail.com>
2020-11-24 19:27:11 +0700

    Nov 24, 2020 -- added about and order route/component done

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,3 +1,45 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 export default function About() {
-  return <div>About</div>
+  return (
+    <div>
+      <Header />
+      <div className="container-fluid d-flex justify-content-center">
+        <div className="row px-3 py-3">
+          <div className="col-sm-5 justify-content-center">
+            <img
+              className="img-fluid img-rounded"
+              src="../images/restaurant/restaurant.jpg"
+              alt="bbL"
+            />
+          </div>
+          <div className="col">
+            <h2>bbL's Restaurant</h2>
+            <p className="text-secondary">
+              Welcome to the Restaurant of bbL. <br />I hope you will enjoy here
+              and if you have some question you can call me 123456789.
+            </p>
+            <p className="text-secondary">
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga. Et harum quidem rerum
+              facilis est et expedita distinctio. Nam libero tempore, cum soluta
+              nobis est eligendi optio cumque nihil impedit quo minus id quod
+              maxime placeat facere possimus, omnis voluptas assumenda est,
+              omnis dolor repellendus. Temporibus autem quibusdam et aut
+              officiis debitis aut rerum necessitatibus saepe eveniet ut et
+              voluptates repudiandae sint et molestiae non recusandae. Itaque
+              earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+              voluptatibus maiores alias consequatur aut perferendis doloribus
+              asperiores repellat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer company="bbL" email="chestprime@gmail.com" />
+    </div>
+  )
 }
--- src/containers/order/Order.js
+++ src/containers/order/Order.js
@@ -1,3 +1,73 @@
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from 'reactstrap'
+
 export default function Order() {
-  return <div>Order</div>
+  const [orders, setOrders] = useState(null)
+
+  useEffect(() => {
+    axios.get('http://localhost:3030/orders').then((res) => setOrders(res.data))
+  }, [])
+
+  //this function, idk why doesn't it work when i call in <div>{showOrders()}</div>
+  // function showOrders() {
+  //   orders &&
+  //     orders.map((order, key) => {
+  //       return <li key={key}>{order.id}</li>
+  //     })
+  // }
+
+  return (
+    <div>
+      <Header />
+      <div className="container-fluid">
+        <h1>Orders</h1>
+        <div className="row">
+          {orders &&
+            orders.map((order, key) => {
+              const date = new Date(order.date)
+              return (
+                <div key={key} className="col-md-3 mb-5">
+                  <Card>
+                    <CardBody>
+                      <CardTitle tag="h5">Order {order.id}</CardTitle>
+                      <CardSubtitle tag="h6" className="mb-2 text-muted">
+                        {date.toLocaleDateString()} |{' '}
+                        {date.toLocaleTimeString()}
+                      </CardSubtitle>
+                      <CardText>
+                        {order.orders.map((record) => {
+                          return (
+                            <ul style={{ listStyleType: 'none' }}>
+                              <li>
+                                {record.product.name} | {record.quantity}
+                              </li>
+                            </ul>
+                          )
+                        })}
+                      </CardText>
+                      <div className="d-flex justify-content-end">
+                        <Button className="btn btn-success mr-3">O</Button>
+                        <Button className="btn btn-danger">X</Button>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </div>
+              )
+            })}
+        </div>
+      </div>
+      <Footer company="bbL" email="chestprime@gmail.com" />
+    </div>
+  )
 }
